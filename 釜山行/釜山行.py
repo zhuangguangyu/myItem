0d4505ce56fc7913e19df74fd8931ab0a2569e90
zhuangguangyu <1905656823@qq.com>
2019-09-03 21:42:10 +0800

    Second

--- 釜山行/釜山行.py
+++ 釜山行/釜山行.py
@@ -12,7 +12,7 @@ names={}           #姓名字典
 relationships={}   #关系字典
 lineNames=[]       #每段内任务关系
 
-jieba.load_userdict("E:/练习/暑假/釜山行/dict.txt")              #加载字典
+jieba.load_userdict("E:/练习/暑假/釜山行/dict.txt")              #加载字典，dict中的nr代表人名
 with codecs.open("E:/练习/暑假/釜山行/busan.txt","r","utf-8") as f:
     for line in f.readlines():
         poss=pseg.cut(line)                                   #分词并返回该词词性
@@ -21,25 +21,25 @@ with codecs.open("E:/练习/暑假/釜山行/busan.txt","r","utf-8") as f:
             if w.flag!="nr" or len(w.word)<2:                 #当分词长度小于2或该词词性不为nr时认为该词不为人名
                 continue
             lineNames[-1].append(w.word)   #为当前段的环境增加一个人物
-            if names.get(w.word) is None:
-                names[w.word]=0
-                relationships[w.word]={}
-            names[w.word]+=1
+            if names.get(w.word) is None:   #如果提取到的姓名不在姓名字典中
+                names[w.word]=0            #设置出现次数为0 
+                relationships[w.word]={}    #设置关系为空
+            names[w.word]+=1                #出现次数加1
 for name,times in names.items():
     print(name,times)
     
-for line in lineNames:   #对于每一段
+for line in lineNames:   #对于每一段中的人名
     for name1 in line:
-        for name2 in line:
+        for name2 in line:        #这两个for循环是对一行中人名的遍历。
             if name1==name2:
                 continue
-            if relationships[name1].get(name2) is None:      #若两人尚未同时出现则新建项
+            if relationships[name1].get(name2) is None:       #若两人尚未同时出现则新建项（如果两个人名不同，且没有两者的关系变量则新建）
                 relationships[name1][name2]=1
             else:
                 relationships[name1][name2]=relationships[name1][name2]+1
                 
-with codecs.open("E:/练习/暑假/釜山行/busan_node.txt","w","gbk") as f:
-    f.write("Id Label Weight\r\n")
+with codecs.open("E:/练习/暑假/釜山行/busan_node.txt","w","gbk") as f:   #打开或者新建一个节点集合文本
+    f.write("Id Label Weight\r\n")      #这一步是为了Gephi的可视化做准备
     for name,times in names.items():
         f.write(name+" "+name+" "+str(times)+"\r\n")
         
@@ -47,7 +47,7 @@ with codecs.open("E:/练习/暑假/釜山行/busan_edge.txt","w","gbk")as f:
     f.write("Source Target Weight\r\n")
     for name,edges in relationships.items():
         for v,w in edges.items():
-            if w > 3:
+            if w > 3:                   #以3为界限去除人物之间的冗余关系，
                 f.write(name+" "+v+" "+str(w)+"\r\n")
         
 
